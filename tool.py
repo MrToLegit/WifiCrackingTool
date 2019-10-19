613ed71dcba934cc73dc44344cfd93838a5b34e7
MrToLegit <MrToLegit@users.noreply.github.com>
2019-10-19 17:40:29 +0200

    Update tool.py

--- tool.py
+++ tool.py
@@ -12,7 +12,7 @@ updateagreeinput = input()
 
 if updateagreeinput == "y" or "Y":
     os.system("git pull origin master")
-    print("Dear Customer,\nplease restart this program.\nSorry for any inconvenience.")
+    print("Dear Customer,\nplease restart this program.\nSorry for any inconveniences.")
     quit()
 
 print("\033[0;0;32mWelcome to the WlanCrack automatic script. Made by ToLegit & MikeMike.\nIf you want to quit press simply CTRL and C. Please follow the steps.\nHave a nice day. \033[0m\n")
