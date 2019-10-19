a285965eaf548467a69ccc78c1df202c2f704cd0
MrToLegit <MrToLegit@users.noreply.github.com>
2019-10-19 20:33:38 +0200

    Update install.py

--- install.py
+++ install.py
@@ -11,13 +11,10 @@ if platform.system() != "Linux":
 print("Welcome to the autoinstaller. Please wait this can take some time....")
 time.sleep(1)
 
-os.system("apt-get install python3")
+os.system("apt-get update -y")
+
+os.system("apt-get install python3 -y")
 
 os.system("python3 -m pip install netifaces")
-os.system("python3 -m pip install os")
-os.system("python3 -m pip install platform")
-os.system("python3 -m pip install time")
-os.system("python3 -m pip install subprocess")
-os.system("python3 -m pip install webbrowser")
 
 print("Succesfully installed everything!\nNow you can run the program with 'python3 tool.py'")
